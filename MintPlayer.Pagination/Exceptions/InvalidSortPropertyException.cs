a4f07f96bdbc18e0aa788c3af877adf7b19d1b49
MusicDemons <pieterjandeclippel@hotmail.com>
2020-05-17 10:29:29 +0200

    Add extension method

---
+++ MintPlayer.Pagination/Exceptions/InvalidSortPropertyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MintPlayer.Pagination.Exceptions
+{
+    public class InvalidSortPropertyException : Exception
+    {
+        internal InvalidSortPropertyException(string propertyName) : base($@"The specified sorting property ""{propertyName}"" does not exist.")
+        {
+        }
+    }
+}
