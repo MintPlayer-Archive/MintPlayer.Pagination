8c3b8be7cf14f10cea4fdae7edbd39e59fc1a60e
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2022-06-22 13:24:15 +0200

    Add mapping project

---
+++ dotnet/MintPlayer.Mapping/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MintPlayer.Mapping.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IMapper<TSource, TTarget> GetMapper<TSource, TTarget>(this IServiceProvider services)
+        {
+            return services.GetRequiredService<IMapper<TSource, TTarget>>();
+        }
+    }
+}
--- dotnet/MintPlayer.Pagination/Extensions/IQueryableExtensions.cs
+++ dotnet/MintPlayer.Pagination/Extensions/IQueryableExtensions.cs
@@ -1,17 +1,66 @@
-﻿using System.Linq;
+﻿using MintPlayer.Mapping;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace MintPlayer.Pagination.Extensions
 {
     public static class IQueryableExtensions
     {
+        /// <summary>Sort ascending based on a string.</summary>
+        /// <param name="query">Input queryable</param>
+        /// <param name="propertyName">Name of the property to sort on.</param>
         public static IOrderedQueryable<TSource> OrderBy<TSource>(this IQueryable<TSource> query, string propertyName)
         {
             return query.OrderByBase(propertyName, true);
         }
+
+        /// <summary>Sort descending based on a string.</summary>
+        /// <param name="query">Input queryable</param>
+        /// <param name="propertyName">Name of the property to sort on.</param>
         public static IOrderedQueryable<TSource> OrderByDescending<TSource>(this IQueryable<TSource> query, string propertyName)
         {
             return query.OrderByBase(propertyName, false);
+        }
+
+        /// <summary>Applies sorting + paging on the queryable.</summary>
+        /// <typeparam name="TDto">DTO type</typeparam>
+        /// <typeparam name="TEntity">Entity type</typeparam>
+        /// <param name="source">DbSet or Queryable</param>
+        /// <param name="request">Pagination request</param>
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> source, PaginationRequest<T> request)
+        {
+            // 1) Sort
+            var orderedItems = request.SortDirection == System.ComponentModel.ListSortDirection.Descending
+                ? source.OrderByDescending(request.SortProperty)
+                : source.OrderBy(request.SortProperty);
+
+            // 2) Page
+            var pagedItems = orderedItems
+                .Skip((request.Page - 1) * request.PerPage)
+                .Take(request.PerPage);
+
+            return pagedItems;
+        }
+
+
+        public static async Task<PaginationResponse<TDto>> Paginate<TDto, TEntity>(this IQueryable<TEntity> source, PaginationRequest<TDto> request, IMapper<TEntity, TDto> mapper)
+        {
+            // 1) Sort
+            var orderedItems = request.SortDirection == System.ComponentModel.ListSortDirection.Descending
+                ? source.OrderByDescending(request.SortProperty)
+                : source.OrderBy(request.SortProperty);
+
+            // 2) Page
+            var pagedItems = orderedItems
+                .Skip((request.Page - 1) * request.PerPage)
+                .Take(request.PerPage);
+
+            // 3) Convert to DTO
+            var dtoItems = (await Task.WhenAll(pagedItems.Select(item => mapper.Map(item)))).ToList();
+
+            var countItems = source.Count();
+            return new PaginationResponse<TDto>(request, countItems, dtoItems);
         }
 
         private static IOrderedQueryable<TSource> OrderByBase<TSource>(this IQueryable<TSource> query, string propertyName, bool ascending)
